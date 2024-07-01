3b82aa868e9f0891a4e412e6adf67a36d7d5ae1a
brenosantin96 <brenolimaa96@gmail.com>
2024-07-01 20:05:14 +0200

    created Register Route

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -2,12 +2,14 @@ import { Router } from 'express';
 
 import * as UploadController from '../controllers/UploadController'
 import * as TestController from '../controllers/UserController';
+import * as LoginAndRegister from '../controllers/UserController';
 
 import { upload } from '../utilities/multerConfig';
 
 const router = Router();
 
 router.get('/api/ping', TestController.ping);
+router.post('/api/register', LoginAndRegister.createUser);
 router.post('/api/upload', upload.single('excel'), UploadController.postFile, UploadController.readFile)
 router.get('/api/readupload', UploadController.readFile)
 
