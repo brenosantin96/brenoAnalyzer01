f0e12912a4488cc2a70fb575be24458112af0e50
Breno Santin <brenolimaa96@gmail.com>
2024-07-11 21:02:37 +0200

    added new route

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import * as UploadController from '../controllers/UploadController'
 import * as TestController from '../controllers/UserController';
 import * as LoginAndRegister from '../controllers/UserController';
+import * as LoginController from '../controllers/LoginAndRegisterController';
 
 import { upload } from '../utilities/multerConfig';
 
@@ -10,6 +11,7 @@ const router = Router();
 
 router.get('/api/ping', TestController.ping);
 router.post('/api/register', LoginAndRegister.createUser);
+router.post('/api/login', LoginController.login);
 router.post('/api/upload', upload.single('excel'), UploadController.postFile, UploadController.readFile)
 router.get('/api/readupload', UploadController.readFile)
 
