93e5be948ba95ccc62864fcdbc6627ebae660477
Breno Santin <brenolimaa96@gmail.com>
2025-02-04 23:17:08 +0100

    new Changes

---
+++ prisma/migrations/20250204210812_first/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE `inc_vs_ritm_texts` MODIFY `casuistry` TEXT NULL,
+    MODIFY `type_spanish` MEDIUMTEXT NULL,
+    MODIFY `type_english` MEDIUMTEXT NULL;
--- src/controllers/UploadController.ts
+++ src/controllers/UploadController.ts
@@ -22,28 +22,21 @@ export const postFile: RequestHandler = async (req, res, next) => {
 }
 
 export const readFile: RequestHandler = async (req, res, next) => {
-
-
     if (!req.file) {
-        return res.json({ error: "File needs to be uploaded" })
+        return res.json({ error: "File needs to be uploaded" });
     }
 
-    const workbook = xlsx.readFile(`./uploads/${req.file.filename}`)
+    const workbook = xlsx.readFile(`./uploads/${req.file.filename}`);
     let workbook_sheet = workbook.SheetNames;
 
     let workbook_response = xlsx.utils.sheet_to_json(
         workbook.Sheets[workbook_sheet[0]]
     );
 
-
     // Verificando se workbook_response[0] é um objeto
     if (typeof workbook_response[0] !== 'object' || workbook_response[0] === null) {
         return res.json({ error: "Invalid data format" });
     }
-
-    // Obtendo o cabeçalho
-    const header = Object.keys(workbook_response[0]);
-
 
     // Função para formatar a data no formato desejado (ano-mês-dia hora:minuto:segundo)
     const formatDate = (dateNumber: number) => {
@@ -52,94 +45,58 @@ export const readFile: RequestHandler = async (req, res, next) => {
     };
 
 
-    //criterios para ser considerado um excel de incidencias
-    let tableWithAllColumns = workbook_response.find((row: any) =>
-        row.Abierto &&
-        row["Actualizado"] || row["Actualizados"] &&
-        row["Asignado a"] &&
-        row["Número"] &&
-        row.Etiquetas &&
-        row["Motivo para poner en espera"] || row["Razón Pendiente"]
-    )
+    // Obtendo o cabeçalho
+    const header = Object.keys(workbook_response[0]);
 
-    //entra nesse if se for um .xls de CHATs importado
-    if (!tableWithAllColumns) {
+    // Normalizando o cabeçalho: substituindo "actualizados" por "actualizado"
+    const normalizedHeader = header.map(column =>
+        column.toLowerCase() === "actualizados" ? "actualizado" : column
+    );
 
-        let chatIncidentsTable = workbook_response.find((row: any) =>
-            row.Creado &&
-            row.Tarea &&
-            row["Asignado a"] &&
-            row["Porcentaje de negocio trascurrido"]
-        )
+    // Atualizando os dados com o cabeçalho normalizado
+const normalizedData = workbook_response.map((row: any) => {
+    const newRow: any = {};
+    header.forEach((column, index) => {
+        const normalizedColumn = normalizedHeader[index];
+        newRow[normalizedColumn] = row[column];
+    });
 
-        if (!chatIncidentsTable) {
-            return res.json({ err: "For incidents, you should import a .xls file that has the columns: 'asignado a', 'abierto', 'actualizado', 'etiquetas', 'numero', 'motivo para poner en espera'. For chats, you should import a ..xls file that has the columns: 'Tarea', 'Asignado a', 'Porcentaje de negocio trascurrido'" })
-        }
-
-        //will only enter here if have columns of chat.
-        if (chatIncidentsTable) {
-
-
-            // Iterando sobre os dados e formatando as datas
-            workbook_response = workbook_response.map((row: any) => {
-                // Verificando se as chaves "Creado" existem e são números
-                if (typeof row.Creado === 'number') {
-                    row.Creado = formatDate(row.Creado);
-                }
-                return row;
-            });
-
-
-            // Removendo espaços em branco dos nomes das colunas
-            const formattedData = workbook_response.map((row: any) => {
-                const newRow: any = {};
-                header.forEach(column => {
-                    const formattedKey = accents.remove(column.replace(/\s/g, '').toLowerCase());
-                    newRow[formattedKey] = row[column];
-                });
-                return newRow;
-            });
-
-            return res.status(200).json({ message: formattedData });
-
-
-        }
-
+    // Aplicando a função formatDate aos campos de data
+    if (typeof newRow.abierto === 'number') {
+        newRow.abierto = formatDate(newRow.abierto);
+    }
+    if (typeof newRow.actualizado === 'number') {
+        newRow.actualizado = formatDate(newRow.actualizado);
     }
 
+    return newRow;
+});
+    
+
+    
     // Iterando sobre os dados e formatando as datas
-    workbook_response = workbook_response.map((row: any) => {
-        // Verificando se as chaves "Abierto" e "Actualizado" existem e são números
+    const formattedData = normalizedData.map((row: any) => {
         if (typeof row.Abierto === 'number') {
             row.Abierto = formatDate(row.Abierto);
         }
         if (typeof row.Actualizado === 'number') {
             row.Actualizado = formatDate(row.Actualizado);
         }
-        if (typeof row["Actualizados"] === 'number') {
-            row["Actualizados"] = formatDate(row["Actualizados"]);
-        }
         return row;
     });
 
-
     // Removendo espaços em branco dos nomes das colunas
-    const formattedData = workbook_response.map((row: any) => {
+    const finalData = formattedData.map((row: any) => {
         const newRow: any = {};
-        header.forEach(column => {
+        normalizedHeader.forEach(column => {
             const formattedKey = accents.remove(column.replace(/\s/g, '').toLowerCase());
             newRow[formattedKey] = row[column];
         });
         return newRow;
     });
 
-    return res.status(200).json({ message: formattedData });
-
-
-
-    //return res.status(200).json({message: workbook_response})
-
-}
+    return res.status(200).json({ message: finalData });
+};
 
 
 //const removeBlankSpaces = ()
